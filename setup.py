9d4c639069fdac0205b8365501b677f008163086
Ansgar Wehrhahn <ansgar.wehrhahn@physics.uu.se>
2020-08-24 12:53:50 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ import versioneer
 
 setup(
     name="flex-format",
+    url="https://github.com/AWehrhahn/flex-format",
+    author="Ansgar Wehrhahn",
+    author_email="ansgar.wehrhahn@physics.uu.se",
     packages=find_packages(),
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
